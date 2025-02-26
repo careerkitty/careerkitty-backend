67f9f2cc6bf3e7eb54453c82c28c774674bd93e1
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2025-02-26 22:48:54 +0200

    feat: authentication

--- jobmatcher/auth_app/serializers.py
+++ jobmatcher/auth_app/serializers.py
@@ -4,4 +4,4 @@ class UserSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
     name = serializers.CharField(max_length=255, required=False)
-    resume_file = serializers.FileField(required=False)
+    resume_file = serializers.FileField(required=True)
